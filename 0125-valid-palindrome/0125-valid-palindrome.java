c954694ae85f7e93bd537141766ffb20a0ea2195
mayuri <84724511+mayuriswan@users.noreply.github.com>
2024-11-23 13:10:24 +0100

    Time: 2 ms (99.09%), Space: 42.4 MB (97.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,32 @@
 class Solution {
     public boolean isPalindrome(String s) {
-            String s1 = s.toLowerCase();
-            String s2 = s1.replaceAll("[^a-z0-9]","");
-            char [] car = s2.toCharArray();
-            int right = s2.length()-1;
-            int left = 0 ; 
-            while(left<right){
-                if(car[left++]!=car[right--]){
-                    return false ;
-                }
-            }
-            return true ; 
+            // String s1 = s.toLowerCase();
+            // String s2 = s1.replaceAll("[^a-z0-9]","");
+            // char [] car = s2.toCharArray();
+            // int right = s2.length()-1;
+            // int left = 0 ; 
+            // while(left<right){
+            //     if(car[left++]!=car[right--]){
+            //         return false ;
+            //     }
+            // }
+            // return true ; 
+             int left = 0, right = s.length() - 1;
+    while (left < right) {
+        // Skip non-alphanumeric characters
+        while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+            left++;
+        }
+        while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            right--;
+        }
+        // Compare characters (ignoring case)
+        if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
     }
 }
